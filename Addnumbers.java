f9990d2ea4189830cc5776acb53fef37807dfb7d
siri <malavathshirisha0@gmail.com>
2025-04-17 17:02:35 +0530

    add extra code

--- Addnumbers.java
+++ Addnumbers.java
@@ -6,8 +6,9 @@ public class Addnumbers
     {
         int n1=10;
         int n2=12;
+        int n3=11;
         int sum=0;
-       sum=n1+n2;
+       sum=n1+n2+n3;
        System.out.println(sum);
     
     }
