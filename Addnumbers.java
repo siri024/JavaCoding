55969d0056e07b6e24766a7f822e2395167f088a
siri <malavathshirisha0@gmail.com>
2025-04-17 16:43:46 +0530

    write the code

---
+++ Addnumbers.java
@@ -0,0 +1,14 @@
+package javafile;
+
+public class Addnumbers 
+{
+    public static void main(String[] args) 
+    {
+        int n1=10;
+        int n2=12;
+        int sum=0;
+       sum=n1+n2;
+       System.out.println(sum);
+    
+    }
+}
